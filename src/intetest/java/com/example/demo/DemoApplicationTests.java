90147abadfb3ad2deee261f03b5dde18b85927c5
FrederikCampagne <frederikcampagne@gmail.com>
2018-12-20 13:26:40 +0100

    abstracted chromedriver

--- src/intetest/java/com/example/demo/DemoApplicationTests.java
+++ src/intetest/java/com/example/demo/DemoApplicationTests.java
@@ -1,42 +1,25 @@
 package com.example.demo;
 
-import java.io.File;
+
 import java.io.IOException;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DemoApplicationTests {
-	 private static ChromeDriverService service;
-	   private WebDriver driver;
+	   private static WebDriver driver;
 
 	   @BeforeClass
 	   public static void createAndStartService() throws IOException {
-	     service = new ChromeDriverService.Builder()
-	         .usingDriverExecutable(new File("chromedriver/chromedriver"))
-	         .usingAnyFreePort()
-	         .build();
-	     service.start();
-	   }
-
-	   @AfterClass
-	   public static void createAndStopService() {
-	     service.stop();
-	   }
-
-	   @Before
-	   public void createDriver() {
-	     driver = new RemoteWebDriver(service.getUrl(),
-	         DesiredCapabilities.chrome());
+	     WebDriverManager.chromedriver().setup();
+	     driver = new ChromeDriver();
 	   }
 
 	   @After
@@ -47,7 +30,8 @@ public class DemoApplicationTests {
 	
 	@Test
 	public void test2() throws InterruptedException {
-		driver.get("http://localhost:8090/HelloWorld");
+	    // fake commit
+		driver.get("http://localhost:8090/tainnovationteam/");
 		Assert.assertEquals("Welcome to HelloWorld!", driver.findElement(By.className("whatup")).getText());
 		 
 	}
