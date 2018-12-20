deae6568515e0293d285ae65ea562dd854478130
FrederikCampagne <frederikcampagne@gmail.com>
2018-12-20 12:13:16 +0100

    ja, paths he

--- src/intetest/java/com/example/demo/DemoApplicationTests.java
+++ src/intetest/java/com/example/demo/DemoApplicationTests.java
@@ -22,7 +22,7 @@ public class DemoApplicationTests {
 	   @BeforeClass
 	   public static void createAndStartService() throws IOException {
 	     service = new ChromeDriverService.Builder()
-	         .usingDriverExecutable(new File("chromedriver/chromedriver.exe"))
+	         .usingDriverExecutable(new File("./chromedriver/chromedriver.exe"))
 	         .usingAnyFreePort()
 	         .build();
 	     service.start();
